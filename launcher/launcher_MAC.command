70df48c027aaa390dbdefa29cf1238d4a9940903
Daniel Weipert <code@drogueronin.de>
2023-08-27 20:51:56 +0200

    Adjust Mac and Windows launcher

--- launcher/launcher_MAC.command
+++ launcher/launcher_MAC.command
@@ -2,11 +2,17 @@ cd "$( dirname "$0" )"
 
 if ! type -p java > /dev/null 2>&1; then
   echo "Java is not installed."
+
+  echo "Press Enter to exit..."
+  read -s
   exit
 fi
 
 if [[ ! $(java --version) == *"64-Bit"* ]]; then
-  echo "Wrong Java version: Java 32-Bit instead of 64-Bit is installed."
+  echo "Wrong Java version: No Java or Java 32-Bit instead of 64-Bit is installed."
+
+  echo "Press Enter to exit..."
+  read -s
   exit
 fi
 
--- launcher/launcher_WINDOWS.bat
+++ launcher/launcher_WINDOWS.bat
@@ -4,17 +4,20 @@ cd /d "%~dp0"
 
 where java >nul 2>nul
 if %errorlevel%==1 (
-    @echo Java is not installed.
-    exit
+    echo Java is not installed.
+
+    goto press_key_to_exit
 )
 
-FOR /F "delims=" %i IN ('java -version') DO set java_version=%i
-if "%java_version%"=="%java_version:64-Bit=%" (
-    @echo Wrong Java version: Java 32-Bit instead of 64-Bit is installed.
-    exit
+java -version 2>&1 | findstr /i "64-Bit" >nul
+if %errorlevel%==1 (
+    echo Wrong Java version: Java 32-Bit instead of 64-Bit is installed.
+
+    goto press_key_to_exit
 )
 
 java -Xmx4608M -jar PokeRandoZX.jar please-use-the-launcher
 
+:press_key_to_exit
 echo Press any key to exit...
 pause >nul
